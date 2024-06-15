e477d04011377bba570573c1cfdf56b17afdae13
juanmamures87 <juanmamures@gmail.com>
2024-06-15 19:55:44 +0200

    Arreglo selección indicadores de página navbar

---
+++ proyecto_web_final/proyecto_inicio_app/static/proyecto_inicio_app/js/scriptPersonalizado.js
@@ -0,0 +1,18 @@
+// Obtiene todos los elementos 'nav-item'
+let navItems = document.querySelectorAll(".nav-item");
+
+// Elimina la clase 'active' de todos los elementos 'nav-item'
+navItems.forEach(function (navItem) {
+  navItem.classList.remove("active");
+});
+
+// Obtiene la ruta de la URL actual
+let rutaActual = window.location.pathname;
+
+// Añade la clase 'active' al elemento 'nav-item' que corresponde a la URL actual
+navItems.forEach(function (navItem) {
+  let anchor = navItem.querySelector("a");
+  if (anchor.getAttribute("href") === rutaActual) {
+    navItem.classList.add("active");
+  }
+});
