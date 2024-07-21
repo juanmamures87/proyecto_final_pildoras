dc5fb39ca9f7d125385138cdff1add94c17bbd8e
juanmamures87 <juanmamures@gmail.com>
2024-07-21 23:28:54 +0200

    Add modelo blog

--- proyecto_web_final/proyecto_blog_app/models.py
+++ proyecto_web_final/proyecto_blog_app/models.py
@@ -1,15 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Blog (models.Model):
-    titulo = models.CharField(max_length=50)
-    contenido = models.TextField()
-    imagen = models.ImageField()
+class Categoria (models.Model):
+    nombre = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
     
     class Meta:
-        verbose_name = "blog"
-        verbose_name_plural = "blogs"
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+        
+    def __str__(self) -> str:
+        return self.nombre
+
+class Post (models.Model):
+    titulo = models.CharField(max_length=50)
+    contenido = models.TextField()
+    imagen = models.ImageField(null=True, blank=True)
+    autor=models.ForeignKey(User,on_delete=models.CASCADE)
+    categorias=models.ManyToManyField(Categoria)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
         
     def __str__(self) -> str:
         return self.titulo
--- proyecto_web_final/proyecto_blog_app/views.py
+++ proyecto_web_final/proyecto_blog_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from proyecto_blog_app.models import Blog
+from proyecto_blog_app.models import Post
 
 def blog (request):
     return render(request, 'proyecto_blog_app/blog.html')
